9d03433a266bf3bafd4174b56307d7a036978917
ivancekikj <ivan.cekic10@gmail.com>
2024-11-28 00:03:30 +0100

    Removed some views and urls

--- foods/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import food
-
-urlpatterns = [
-    path("<str:foodName>", food, name="food")
-]
--- project/urls.py
+++ project/urls.py
@@ -5,8 +5,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls")),
-    path("accounts/", include("accounts.urls")),
-    path("order", include("orders.urls")),
-    path("foods/", include("foods.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
