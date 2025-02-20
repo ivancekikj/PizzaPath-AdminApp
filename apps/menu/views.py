02609876b069a9e4566b0bca50827a29003c2039
ivancekikj <ivan.cekic10@gmail.com>
2025-02-20 18:19:30 +0100

    View set for category and food

--- apps/menu/views.py
+++ apps/menu/views.py
@@ -1,1 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from apps.menu.models import Food, Category
+from apps.menu.serializers import CategorySerializer, FoodSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    http_method_names = ["get",]
+
+
+class FoodViewSet(viewsets.ModelViewSet):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+
+    http_method_names = ["get",]
+
+    def list(self, request, *args, **kwargs):
+        category_id = request.query_params.get('category_id', None)
+        if category_id is None:
+            return Response("category_id is required", status=400)
+        foods = Food.objects.filter(category_id=category_id)
+        serialized_data = FoodSerializer(foods, many=True).data
+        return Response(serialized_data, status=200)
--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+from apps.menu.views import CategoryViewSet, FoodViewSet
+
+router = DefaultRouter()
+router.register(r"menu/categories", CategoryViewSet)
+router.register(r"menu/foods", FoodViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
