8c51e729fa168cc958778aee8f2318f9f4f1e50e
ivancekikj <ivan.cekic10@gmail.com>
2025-04-13 13:09:14 +0200

    Add item to order

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,18 +1,43 @@
-from django.shortcuts import render, redirect
-from apps.menu.models import Category, Food
+from datetime import datetime
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from apps.menu.models import FoodPortion, Topping
+from apps.orders.models import Order, OrderItem
 
 
-def order(request):
-    if request.method == "POST":
-        return render(request, "pages/index.html")
-    else:
-        foods = {}
-        categories = []
-        for category in Category.objects.all():
-            foods[category.name.capitalize()] = Food.objects.filter(category=category)
-            categories.append(category.name.capitalize())
-        context = {
-            "foods": foods,
-            "categories": categories
-        }
-        return render(request, "orders/order.html", context)
+class OrderItemView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        quantity = request.data.get('quantity', None)
+        food_portion_id = request.data.get('food_portion_id', None)
+        toppings_ids = request.data.get('toppings_ids', [])
+        user_id = request.user.id
+
+        if quantity is None:
+            return Response("quantity expected.", status=400)
+        if food_portion_id is None:
+            return Response("food_portion_id expected.", status=400)
+
+        order = Order.objects.filter(customer_id=user_id).exclude(status="closed").first()
+        food_portion = FoodPortion.objects.get(id=food_portion_id)
+        toppings = Topping.objects.filter(id__in=toppings_ids) if toppings_ids and len(toppings_ids) > 0 else []
+        if order is None:
+            order = Order.objects.create(customer_id=user_id, date_time_edited=datetime.now())
+            order.save()
+        order_item = OrderItem.objects.create(
+            quantity=quantity,
+            discount = food_portion.discount,
+            price=food_portion.price,
+            order=order,
+            food_portion=food_portion,
+        )
+        order_item.save()
+        order_item.toppings.add(*toppings)
+        order.date_time_edited = datetime.now()
+        order_item.save()
+
+        return Response(status=200)
--- project/urls.py
+++ project/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import DefaultRouter
 
 from apps.accounts.views import CustomerView, LogoutView, LoginView
 from apps.menu.views import CategoryViewSet, FoodViewSet, FoodPortionView
+from apps.orders.views import OrderItemView
 
 router = DefaultRouter()
 router.register(r"menu/categories", CategoryViewSet)
@@ -15,6 +16,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/menu/food-portions/', FoodPortionView.as_view()),
+    path('api/orders/current-order/items/', OrderItemView.as_view()),
     path('api/accounts/customers/', CustomerView.as_view()),
     path('api/accounts/login/', LoginView.as_view()),
     path('api/accounts/logout/', LogoutView.as_view()),
