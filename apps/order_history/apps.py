0a542e285ef20e5fd486252a539052864fd7084d
ivancekikj <ivan.cekic10@gmail.com>
2025-04-18 13:43:27 +0200

    Update app config

--- apps/order_history/apps.py
+++ apps/order_history/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OrderHistoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'order_history'
+    name = 'apps.order_history'
