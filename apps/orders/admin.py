8e9a6063daef62d0ba8c999483823d71490e6569
ivancekikj <ivan.cekic10@gmail.com>
2025-05-19 14:09:20 +0200

    Number of items and total cost for order in admin panel

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,9 +1,11 @@
+import math
+
 from django.contrib import admin
 from .models import Order, OrderItem
 
 class OrderAdmin(admin.ModelAdmin):
     fieldsets = (
-        ("General Info", {"fields": ("order_number", "customer", "date_time_edited", "status", "description")}),
+        ("General Info", {"fields": ("order_number", "customer", "date_time_edited", "status", "description", "number_of_items", "total_price")}),
         ("Order Items", {"fields": ()}),
     )
     list_display = ("customer", "status", "date_time_edited",)
@@ -13,6 +15,21 @@ class OrderAdmin(admin.ModelAdmin):
     def order_number(self, obj):
         return obj.id
 
+    def total_price(self, obj):
+        total = 0
+        for item in obj.orderitem_set.all():
+            item_cost = item.food_portion.price
+            for topping in item.toppings.all():
+                item_cost += topping.price
+            item_cost *= item.quantity
+            if item.food_portion.discount > 0:
+                item_cost *= (1 - item.food_portion.discount)
+            total += math.ceil(item_cost)
+        return f"{total} ден"
+
+    def number_of_items(self, obj):
+        return len(obj.orderitem_set.all())
+
     def has_add_permission(self, request):
         return False
 
@@ -20,7 +37,7 @@ class OrderAdmin(admin.ModelAdmin):
         return False
 
     def get_readonly_fields(self, request, obj=None):
-        return ['order_number', 'description', 'customer', 'date_time_edited']
+        return ['order_number', 'description', 'customer', 'date_time_edited', 'total_price', 'number_of_items']
 
 
 admin.site.register(Order, OrderAdmin)
