70b9c2454c720a968b754abbbf4c702d531e4f2a
ivancekikj <ivan.cekic10@gmail.com>
2025-07-09 23:21:18 +0200

    Fetch all user coupons and order coupon info

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.accounts.models import CouponReward
+from apps.accounts.serializers import CouponRewardSerializer
 from apps.menu.models import FoodPortion, Topping
 from apps.orders.models import Order, OrderItem
 from apps.orders.serialiers import OrderSerializer
@@ -141,6 +143,28 @@ class CurrentOrderView(APIView):
         order.delete()
 
         return Response(status=200)
+
+
+class OrderCouponRewardView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        user_id = request.user.id
+        order = get_current_order(user_id)
+        if order is None:
+            return Response(None, status=200)
+        earned_coupons, redeemed_coupons = 0, 0
+        coupons = CouponReward.objects.filter(customer_id=user_id)
+        order_items = OrderItem.objects.filter(order__customer_id=user_id,are_coupons_used=True)
+        for item in order_items:
+            redeemed_coupons += item.quantity * item.food_portion.coupon_value
+            earned_coupons += item.quantity
+            coupons.get(food_portion_id=item.food_portion_id).count -= item.quantity * item.food_portion.coupon_value
+        return Response({
+            "coupons": CouponRewardSerializer(coupons, many=True).data,
+            "earned_coupons": earned_coupons,
+            "redeemed_coupons": redeemed_coupons,
+        }, status=200)
 
 
 def check_if_editable(order: Order):
--- project/urls.py
+++ project/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
-from apps.accounts.views import CustomerView, LogoutView, LoginView
+from apps.accounts.views import CustomerView, LogoutView, LoginView, CouponRewardView, CurrentCustomerView
 from apps.menu.views import CategoryViewSet, FoodViewSet, FoodPortionView
-from apps.orders.views import OrderItemView, CurrentOrderView
+from apps.orders.views import OrderItemView, CurrentOrderView, OrderCouponRewardView
 
 router = DefaultRouter()
 router.register(r"menu/categories", CategoryViewSet)
@@ -19,7 +19,10 @@ urlpatterns = [
     path('api/orders/current-order/items/', OrderItemView.as_view()),
     path('api/orders/current-order/items/<int:id>/', OrderItemView.as_view()),
     path('api/orders/current-order/', CurrentOrderView.as_view()),
+    path('api/orders/current-order/coupon-info', OrderCouponRewardView.as_view()),
     path('api/accounts/customers/', CustomerView.as_view()),
+    path('api/accounts/customers/logged-in-customer/', CurrentCustomerView.as_view()),
+    path('api/accounts/customers/logged-in-customer/coupons/', CouponRewardView.as_view()),
     path('api/accounts/login/', LoginView.as_view()),
     path('api/accounts/logout/', LogoutView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
