16fb76d2622e5c6ed4dd0381944bd01d7bc7bef7
ivancekikj <ivan.cekic10@gmail.com>
2025-07-15 22:19:20 +0200

    22-include-coupons-in-order Remove outdated user coupon query

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -11,18 +11,9 @@ class CouponRewardSerializer(serializers.ModelSerializer):
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    coupons = serializers.SerializerMethodField()
-
     class Meta:
         model = Customer
-        fields = ['username', 'email', 'first_name', 'last_name', 'address', 'phone_number', 'password', 'is_subscribed_to_newsletter', 'coupons']
-
-    def get_coupons(self, obj):
-        coupons = CouponReward.objects.filter(customer_id=obj.id)
-        order_items = OrderItem.objects.filter(order__customer_id=obj.id,are_coupons_used=True)
-        for item in order_items:
-            coupons.get(food_portion_id=item.food_portion_id).count -= item.quantity * item.food_portion.coupon_value
-        return CouponRewardSerializer(coupons, many=True).data
+        fields = ['username', 'email', 'first_name', 'last_name', 'address', 'phone_number', 'password', 'is_subscribed_to_newsletter']
 
     def create(self, data):
         user = Customer.objects.create_user(**data)
